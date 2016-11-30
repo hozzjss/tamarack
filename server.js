0de76f132b4333c0025bf99d141ff761d5021d96
hozzj <medo3578@gmail.com>
2016-12-01 01:15:44 +0200

    version 0.0.2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.get('/scrape', function (req, res) {
 
@@ -25,6 +26,6 @@ app.get('/scrape', function (req, res) {
 	});
 })
 
-app.listen('8081')
-console.log('Magic happens on port 8081');
+app.listen(port)
+	//console.log('Magic happens on port 8081');
 exports = module.exports = app;
