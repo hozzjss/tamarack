e24628481bbbd40232f5fe3deab6f74133d406aa
Muhammad Huzayen <medo3578@gmail.com>
2017-02-12 05:57:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,44 +5,62 @@ const url = require('url')
 const app = express()
 const port = process.env.PORT || 8080
 
-app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Methods', 'GET')
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-	res.setHeader('Access-Control-Allow-Credentials', true)
-	next()
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
 })
 
 app.get('/', function(req, res) {
-	res.send("Usage: /scrape or /weather")
+  res.send("Usage: /scrape")
 })
 
-app.get('/scrape', function (req, res) {
+app.get('/scrape', function(req, res) {
 
-	let query = req.query.query
+  let query = req.query.query
 
-	request(query, function (error, response, html) {
-		let raised = "Error!"
-		if (!error) {
-			let $ = cheerio.load(html)
-			raised = $('.fundraiserProgress-current').text()
-			if (raised.indexOf("goal") !== -1)
-				raised = "$0"
-		}
-		res.end(raised)
-	});
+  request(query, function(error, response, html) {
+    let raised = "Error!"
+    if (!error) {
+      let $ = cheerio.load(html)
+      raised = $('.fundraiserProgress-current').text()
+      if (raised.indexOf("goal") !== -1)
+        raised = "$0"
+    }
+    res.end(raised)
+  });
 })
 
-app.get('/weather', function (req, res) {
-	let lon = req.query.lon;
-	let lat = req.query.lat;
-	let key = req.query.key;
-	let apiReq = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
-	request(apiReq, function (error, response, html) {
-		if(!error) {
-			res.end(html);
-		}
-	});
+app.get('/weather', function(req, res) {
+  let lon = req.query.lon;
+  let lat = req.query.lat;
+  let apiReq = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=560c314416cee6b04950e1f5415da8c5`;
+  request(apiReq, function(error, response, html) {
+    if (!error) {
+      console.log(html);
+      res.end(html);
+    }
+  });
 });
+
+app.get('/search', function(req, res) {
+  let searchQuery = req.query.query;
+  let options = {
+    url: 'https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=' + searchQuery,
+    method: 'GET',
+    headers: {
+      'Ocp-Apim-Subscription-Key': '9c09fca80699418ebbca8bfb92adc629'
+    }
+  };
+	request(options, function (error, response, body) {
+		if (!error) {
+			console.log(body);
+			res.end(body);
+		}
+	})
+})
 app.listen(port)
+console.log("app running on port", port);
 exports = module.exports = app;
